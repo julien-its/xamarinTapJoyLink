206beff96e0445197a2ac96f5ba26b24054fd9d2
julien <contact@julien-gustin.be>
2018-07-31 18:52:10 +0800

    troubleshooting

---
+++ TapJoyLink/Additions/TapjoyCache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Com.Tapjoy
+{
+    public partial class TapjoyCache
+    {
+        public partial class CacheAssetThread
+        {
+
+            public Java.Lang.Object Call()
+            {
+                return (Java.Lang.Object)this.BoolCall();
+            }
+        }
+    }
+}
+
---
+++ TapJoyLink/Additions/TapjoyConnectCore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Com.Tapjoy
+{
+    public partial class ITJVideoListenerErrorEventArgs
+    {
+        public void JITSVideoListenerErrorEventArgs(int p0)
+        {
+            this.p0 = p0;
+        }
+
+        int p0;
+        public int P0
+        {
+            get { return p0; }
+        }
+    }
+}
+
